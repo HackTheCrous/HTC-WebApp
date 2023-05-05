7d5a61b5237580db0153118545731bb89a6d44b2
radulescut <your@email.tld>
2023-05-05 16:47:46 +0200

    background light/dark EventDetails.vue

--- client/HackTheCr*us/src/components/EventDetails.vue
+++ client/HackTheCr*us/src/components/EventDetails.vue
@@ -1,5 +1,7 @@
 <template>
-    <div ref="slidable"  id="event-detail" draggable="true" @touchend="dragEnded" @touchmove="handletouch" @dragstart="dragStarted" @drag="dragHandled" @dragend="dragEnded" :class="this.focus ? 'event-detail focused' : 'unfocused event-detail'" >
+    <div ref="slidable" id="event-detail" draggable="true" @touchend="dragEnded" @touchmove="handletouch"
+         @dragstart="dragStarted" @drag="dragHandled" @dragend="dragEnded"
+         :class="this.focus ? 'event-detail focused' : 'unfocused event-detail'">
         <h2>{{ this.data.summary }}</h2>
         <p>{{ this.data.location }}</p>
         <p v-html="(this.data.description.replaceAll('\n', '<br>')).replaceAll(/^(<br>)*/ig,'')"></p>
@@ -23,35 +25,35 @@ export default {
         focus: Boolean,
     },
     methods: {
-        dragStarted(event){
-          event.dataTransfer.setDragImage(new Image(), 0, 0);
+        dragStarted(event) {
+            event.dataTransfer.setDragImage(new Image(), 0, 0);
         },
 
-        handletouch(event){
+        handletouch(event) {
             event.preventDefault();
             event.stopPropagation();
-            this.$refs.slidable.style.top= `${event.touches[0].clientY}px`;
-            if(event.touches[0].clientY>window.innerHeight*0.8){
+            this.$refs.slidable.style.top = `${event.touches[0].clientY}px`;
+            if (event.touches[0].clientY > window.innerHeight * 0.8) {
                 this.$emit('unfocus', true);
             }
         },
 
-      dragHandled(event) {
-          event.dataTransfer.setData("text/plain", this.data.id);
-          event.dataTransfer.dropEffect= "none";
-          event.dataTransfer.effectAllowed= "none";
-          if(event.screenY>window.innerHeight*0.7){
-              this.$refs.slidable.style.top= `${event.screenY}px`;
+        dragHandled(event) {
+            event.dataTransfer.setData("text/plain", this.data.id);
+            event.dataTransfer.dropEffect = "none";
+            event.dataTransfer.effectAllowed = "none";
+            if (event.screenY > window.innerHeight * 0.7) {
+                this.$refs.slidable.style.top = `${event.screenY}px`;
 
-          }
-          if(event.screenY>window.innerHeight*0.95){
-              this.$emit('unfocus', true);
-          }
-      },
-      dragEnded() {
-          this.$refs.slidable.style.bottom= `0px`;
-          this.$refs.slidable.style.top= `auto`;
-      }
+            }
+            if (event.screenY > window.innerHeight * 0.95) {
+                this.$emit('unfocus', true);
+            }
+        },
+        dragEnded() {
+            this.$refs.slidable.style.bottom = `0px`;
+            this.$refs.slidable.style.top = `auto`;
+        }
     },
     computed: {
         data() {
@@ -96,15 +98,17 @@ export default {
   border-radius: 30px;
   padding-bottom: 70px;
   box-shadow: 0 0 10px var(--color-shadow);
+  backdrop-filter: blur(5px);
+  background: var(--color-background-transparent);
 
+  &.focused {
+    animation: slidein 0.1s ease;
+  }
 
-    &.focused{
-        animation: slidein 0.1s ease;
-    }
-    &.unfocused{
-        animation: slideout 0.1s ease;
-        animation-fill-mode: forwards;
-    }
+  &.unfocused {
+    animation: slideout 0.1s ease;
+    animation-fill-mode: forwards;
+  }
 
   h2 {
     font-size: 30px;
