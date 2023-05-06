02fed8608f4941bfa5f9c510f0ba0a702b0bb488
radulescut <tristan-mihai.radulescu@etu.umontpellier.fr>
2023-05-06 17:11:51 +0200

    access control policy

--- server/index.js
+++ server/index.js
@@ -66,6 +66,12 @@ app.use(passport.session());
 
 app.use(cors());
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+})
+
 
 app.listen({port: 4000}, () => console.log(`Server ready at http://locahost:4000${server.graphqlPath}`));
 
